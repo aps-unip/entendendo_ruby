c10ce70b810d31a82fbaf0f484349f67a91a9713
Jones Vitolo <jsvitolo@gmail.com>
2015-09-18 16:31:58 -0300

    Removendo o pry

--- 14-simbolos-estruturas/executar.rb
+++ 14-simbolos-estruturas/executar.rb
@@ -1,6 +1,6 @@
 require_relative 'filme'
 require_relative 'playlist'
-require 'pry'
+
 
 filme1 = Filme.new("robocop", 9)
 filme2 = Filme.new("vingadores", 9)
@@ -10,6 +10,6 @@ playlist = Playlist.new("jones")
 playlist.adiciona_filmes(filme1)
 playlist.adiciona_filmes(filme2)
 playlist.adiciona_filmes(filme3)
-binding.pry
+
 playlist.tocar(3)
 playlist.mostrar_status
